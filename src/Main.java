d2707a45065c5601ed52fc0127c0e713eb195a61
Eugennoix <eugennoix@outlook.com>
2023-01-23 13:16:25 -0500

    Added basic JFrame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Creates the frame
+        JFrame frame = new JFrame("[App Name Here]");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
