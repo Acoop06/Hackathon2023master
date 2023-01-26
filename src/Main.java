bc34396dea7a5d4dfe2fab7875315fe50a1b6d9d
Acoop06 <coopeand001@hsestudents.org>
2023-01-25 22:49:39 -0500

    ADDED TEXT TO THE SCHREEEN WHOOOOOOOO

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        //Creates the frame
+        //Creates the frame to hold panel and text
         JFrame frame = new JFrame("STAY ON TASK");
 
-        //JPanel
+        //JPanel to hold text and icons and buttons
         JPanel  p = new JPanel();
 
-        //creates label
+        //creates label and adds it to the panel
         JLabel label1 = new JLabel("TEXT I ADDED TEXT TO THE SCREEN WOOOOOOOOOOOOO");
-
         p.add(label1);
 
+        //adds panel to the frame and sets frame size
         frame.add(p);
-
         frame.setSize(500,500);
 
+        //makes frame visible
         frame.setVisible(true);
     }
 }
